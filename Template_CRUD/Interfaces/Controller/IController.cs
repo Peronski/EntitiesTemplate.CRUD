f627f522bf3465195c1a4bf26aa9c2797f8f91ab
Josef Peroni <josef.peroni@gmail.com>
2022-07-04 10:28:27 +0200

    [NEW] Interfaces for Controllers, Retrievers, Persisters and SafeOperator

---
+++ Template_CRUD/Interfaces/Controller/IController.cs
@@ -0,0 +1,29 @@
+﻿using Test_Cinema.Persisters;
+using Test_Cinema.Retrievers;
+
+namespace Test_Cinema.Controllers { 
+    public interface IController<T> {
+        public IRetriever<T> Retriever { get; }
+        public IPersister<T> Persister { get; }
+
+        public T Get(int Id) {
+            return Retriever.Get(Id);
+        }
+
+        public void Add(T product) {
+            Persister.Add(product);
+        }
+
+        public void Update(T product, int id) {
+            Persister.Update(product, id);
+        }
+
+        public void Delete(int id) {
+            Persister.Delete(id);
+        }
+
+        public IEnumerable<T> GetAll() {
+            return Retriever.GetAll();
+        }
+    }
+}
